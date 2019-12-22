bc96e0069e28887098ed07c3d292fdd0e6039c70
CynicalCheddar <xz18677@bristol.ac.uk>
2019-12-22 10:03:28 +0000

    courseweork gol

---
+++ University Projects/Game of life COMPLETE/CW/parralel mk1/gol.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func worker(height int,width int, c chan byte) {
+    world := make([][]byte,height)
+    for i :=range world {
+        world[i] = make([]byte,width)
+    }
+    newWorld := make([][]byte,height-2)
+        for i :=range newWorld {
+            newWorld[i] = make([]byte,width)
+        }
+    for {
+        for y:=0;y<height;y++ {
+            for x:=0;x<width;x++ {
+                world[y][x]=<-c
+            }
+        }
+
+
+        for y := 1; y < height-1; y++ {
+      		for x := 0; x < width; x++ {
+      			// Placeholder for the actual Game of Life logic: flips alive cells to dead and dead cells to alive.
+       			 var neighbors=0
+       			 for i:=-1;i<2;i++ {
+       			    for j:=-1;j<2;j++ {
+                           if(i!=0 || j!=0) {
+                               var yb=y+i
+                               var xb=x+j
+                               if(world[yb][xb]!=0){
+                                   neighbors+=1
+                               }
+                           }
+                       }
+       			 }
+                    if(neighbors==3){
+                        newWorld[y][x] = 1
+                    } else if(neighbors == 2) {
+                        newWorld[y][x] = world[y][x]
+                    } else {
+                        newWorld[y][x] = 0
+                    }
+       	   	}
+       	}
+
+
+       	for y:=1;y<height-1;y++ {
+            for x:=0;x<width;x++ {
+                c<-newWorld[y][x]
+            }
+        }
+    }
+}
+
+// distributor divides the work between workers and interacts with other goroutines.
+func distributor(p golParams, d distributorChans, alive chan []cell) {
+
+	// Create the 2D slice to store the world.
+	world := make([][]byte, p.imageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.imageWidth)
+	}
+
+    newWorld := make([][]byte, p.imageHeight)
+    for i := range world {
+    	newWorld[i] = make([]byte, p.imageWidth)
+    }
+
+	// Request the io goroutine to read in the image with the given filename.
+	d.io.command <- ioInput
+	d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
+
+    workerChannels := make([]chan byte,p.threads)
+    for _,c:=range workerChannels {
+        go worker((p.imageHeight/p.threads)+2,p.imageWidth,c)
+    }
+
+	// The io goroutine sends the requested image byte by byte, in rows.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			val := <-d.io.inputVal
+			if val != 0 {
+				fmt.Println("Alive cell at", x, y)
+				world[y][x] = val
+			}
+		}
+	}
+
+	// Calculate the new state of Game of Life after the given number of turns.
+	for turns := 0; turns < p.turns; turns++ {
+        for i,c:=range workerChannels {
+            for y:=((i)*(p.imageHeight/p.threads))-1;y<((i+1)*(p.imageHeight/p.threads))+1;y++ {
+                for x:=0;x<p.imageWidth;x++ {
+                    c<-world[(y+p.imageHeight)%p.imageHeight][x]
+                }
+            }
+        }
+        for i,c:=range workerChannels {
+            for y:=((i)*(p.imageHeight/p.threads));y<((i+1)*(p.imageHeight/p.threads));y++ {
+                for x:=0;x<p.imageWidth;x++ {
+                    newWorld[y][x]=<-c
+                }
+            }
+        }
+	}
+
+    d.io.command <- ioOutput
+    d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
+
+	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
+	var finalAlive []cell
+	// Go through the world and append the cells that are still alive.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+		    d.io.outputVal<-newWorld[y][x]
+			if newWorld[y][x] != 0 {
+				finalAlive = append(finalAlive, cell{x: x, y: y})
+			}
+		}
+	}
+
+	// Make sure that the Io has finished any output before exiting.
+	d.io.command <- ioCheckIdle
+	<-d.io.idle
+
+	// Return the coordinates of cells that are still alive.
+	alive <- finalAlive
+}
---
+++ University Projects/Game of life COMPLETE/CW/test/gol.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// distributor divides the work between workers and interacts with other goroutines.
+func distributor(p golParams, d distributorChans, alive chan []cell) {
+
+	// Create the 2D slice to store the world.
+	world := make([][]byte, p.imageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.imageWidth)
+	}
+
+    newWorld := make([][]byte, p.imageHeight)
+    for i := range world {
+    	newWorld[i] = make([]byte, p.imageWidth)
+    }
+
+	// Request the io goroutine to read in the image with the given filename.
+	d.io.command <- ioInput
+	d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
+
+	// The io goroutine sends the requested image byte by byte, in rows.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+			val := <-d.io.inputVal
+			if val != 0 {
+				fmt.Println("Alive cell at", x, y)
+				world[y][x] = val
+			}
+		}
+	}
+	// Calculate the new state of Game of Life after the given number of turns.
+	for turns := 0; turns < p.turns; turns++ {
+		for y := 0; y < p.imageHeight; y++ {
+			for x := 0; x < p.imageWidth; x++ {
+				// Placeholder for the actual Game of Life logic: flips alive cells to dead and dead cells to alive.
+				 var neighbors=0
+				 for i:=-1;i<2;i++ {
+				    for j:=-1;j<2;j++ {
+                        if(i!=0 || j!=0) {
+                            var yb=((y+i+p.imageHeight)%p.imageHeight)
+                            var xb=((x+j+p.imageWidth)%p.imageWidth)
+                            if(world[yb][xb]!=0){
+                                neighbors+=1
+                            }
+                        }
+                    }
+				 }
+                if(neighbors==3){
+                    newWorld[y][x] = 1
+                } else if(neighbors == 2) {
+                    newWorld[y][x] = world[y][x]
+                } else {
+                    newWorld[y][x] = 0
+                }
+			}
+		}
+	}
+
+    d.io.command <- ioOutput
+    d.io.filename <- strings.Join([]string{strconv.Itoa(p.imageWidth), strconv.Itoa(p.imageHeight)}, "x")
+    fmt.Println("GOT HERE")
+
+	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
+	var finalAlive []cell
+	// Go through the world and append the cells that are still alive.
+	for y := 0; y < p.imageHeight; y++ {
+		for x := 0; x < p.imageWidth; x++ {
+		    fmt.Println("GOT HEREaaa")
+		    d.io.outputVal<-newWorld[y][x]
+		    fmt.Println("GOT HEREbbbb")
+			if newWorld[y][x] != 0 {
+				finalAlive = append(finalAlive, cell{x: x, y: y})
+
+			}
+		}
+	}
+    fmt.Println("GOT HERE2")
+
+	// Make sure that the Io has finished any output before exiting.
+	d.io.command <- ioCheckIdle
+	<-d.io.idle
+
+	// Return the coordinates of cells that are still alive.
+	alive <- finalAlive
+}
